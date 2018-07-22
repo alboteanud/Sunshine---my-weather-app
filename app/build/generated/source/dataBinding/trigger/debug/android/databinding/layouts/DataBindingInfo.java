f06f3f07e1e01587d1aadf7fee35f6f098fd639e
albot <alboteanud@gmail.com>
2018-07-22 11:58:04 +0300

    Initial commit

---
+++ app/build/generated/source/dataBinding/trigger/debug/android/databinding/layouts/DataBindingInfo.java
@@ -0,0 +1,6 @@
+package android.databinding.layouts;
+
+import android.databinding.BindingBuildInfo;
+
+@BindingBuildInfo(buildId="ce15d9e2-08fc-4f12-a1d8-7a36d0efaa94")
+public class DataBindingInfo {}
