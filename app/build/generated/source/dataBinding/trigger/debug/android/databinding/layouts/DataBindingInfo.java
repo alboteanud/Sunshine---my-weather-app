e2c5b26107dff7502fc52fe4e82650804d589b1d
albot <alboteanud@gmail.com>
2018-07-24 13:35:27 +0300

    solved

---
+++ app/build/generated/source/dataBinding/trigger/debug/android/databinding/layouts/DataBindingInfo.java
@@ -0,0 +1,6 @@
+package android.databinding.layouts;
+
+import android.databinding.BindingBuildInfo;
+
+@BindingBuildInfo(buildId="7f931106-1ca6-4b40-abdb-db1fde7b64b6")
+public class DataBindingInfo {}
