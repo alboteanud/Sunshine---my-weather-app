bb6aae256d046c0edf74fe8716e863bfb4db77f5
danalboteanu <alboteanud@gmail.com>
2019-02-11 23:21:53 +0200

    comit 1

---
+++ app/build/generated/source/dataBinding/trigger/debug/android/databinding/layouts/DataBindingInfo.java
@@ -0,0 +1,6 @@
+package android.databinding.layouts;
+
+import android.databinding.BindingBuildInfo;
+
+@BindingBuildInfo(buildId="4bba772f-c344-4220-a79a-0ec8c11630be")
+public class DataBindingInfo {}
