0882ce685e4c24d0559e802a5f3773522b90e4d6
danalboteanu <alboteanud@gmail.com>
2019-04-11 16:47:43 +0300

    production project

--- app/build/generated/source/buildConfig/debug/com/craiovadata/android/sunshine/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/com/craiovadata/android/sunshine/BuildConfig.java
@@ -5,9 +5,9 @@ package com.craiovadata.android.sunshine;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String APPLICATION_ID = "com.craiovadata.android.sunshine.US.AZ.Scottsdale";
+  public static final String APPLICATION_ID = "com.craiovadata.aus.sydney";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 2;
+  public static final int VERSION_CODE = 5;
   public static final String VERSION_NAME = "1.0";
 }
