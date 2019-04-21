076ce43a36cceb4c1b06f84ed88631a0f2bd92e8
Dan_Alboteanu <alboteanud@gmail.com>
2019-04-21 23:53:54 +0300

    working weather app

--- app/build/tmp/kapt3/stubs/debug/com/craiovadata/android/sunshine/ui/main/CardsAdapter.java
+++ app/build/tmp/kapt3/stubs/debug/com/craiovadata/android/sunshine/ui/main/CardsAdapter.java
@@ -71,10 +71,13 @@ public final class CardsAdapter extends androidx.recyclerview.widget.RecyclerVie
         super();
     }
     
-    @kotlin.Metadata(mv = {1, 1, 15}, bv = {1, 0, 3}, k = 1, d1 = {"\u0000\u0016\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0000\n\u0002\u0010\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\bf\u0018\u00002\u00020\u0001J\u0010\u0010\u0002\u001a\u00020\u00032\u0006\u0010\u0004\u001a\u00020\u0005H&\u00a8\u0006\u0006"}, d2 = {"Lcom/craiovadata/android/sunshine/ui/main/CardsAdapter$Listener;", "", "onCelsiusFarClicked", "", "view", "Landroid/view/View;", "app_debug"})
+    @kotlin.Metadata(mv = {1, 1, 15}, bv = {1, 0, 3}, k = 1, d1 = {"\u0000\u0018\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0000\n\u0002\u0010\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0002\bf\u0018\u00002\u00020\u0001J\u0010\u0010\u0002\u001a\u00020\u00032\u0006\u0010\u0004\u001a\u00020\u0005H&J\u0010\u0010\u0006\u001a\u00020\u00032\u0006\u0010\u0004\u001a\u00020\u0005H&\u00a8\u0006\u0007"}, d2 = {"Lcom/craiovadata/android/sunshine/ui/main/CardsAdapter$Listener;", "", "onCelsiusFarClicked", "", "view", "Landroid/view/View;", "onNewsClicked", "app_debug"})
     public static abstract interface Listener {
         
         public abstract void onCelsiusFarClicked(@org.jetbrains.annotations.NotNull()
+        android.view.View view);
+        
+        public abstract void onNewsClicked(@org.jetbrains.annotations.NotNull()
         android.view.View view);
     }
     
